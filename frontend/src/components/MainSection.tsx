902559da4825b1980a0006bcff4e1356d82ef024
xkollar3 <xkollar3@fi.muni.cz>
2023-08-18 14:30:35 +0200

    changes to site layout

--- frontend/src/components/MainSection.tsx
+++ frontend/src/components/MainSection.tsx
@@ -1,17 +1,14 @@
 import {FC} from "react";
 import {Box, Button, Stack, Typography, useMediaQuery} from "@mui/material";
 import Banner from "../assets/banner.jpg"
-import animals from "../assets/animal_cards.json";
-import {AnimalPaper} from "./AnimalPaper";
 import {useNavigate} from "react-router-dom";
+import {AboutSection} from "./AboutSection";
 
 export const MainSection: FC = () => {
     const navigate = useNavigate();
 
     const isMobile = useMediaQuery((theme: any) => theme.breakpoints.down("sm")
         || theme.breakpoints.down("xs"));
-
-    const animalsToDisplay = isMobile ? animals.slice(0, 1) : animals.slice(0, 2);
 
     return <Box textAlign='center' bgcolor='#f6efe3'>
         <Stack rowGap={1} alignItems='center'>
@@ -36,24 +33,7 @@ export const MainSection: FC = () => {
                     Vítame Vás v Parku s najväčším množstvom mačkovitých šeliem v Európe!
                 </Typography>
             </Box>
-            <Button onClick={() => navigate('/opening-hours')} sx={{
-                bgcolor: '#f8b957',
-                borderRadius: '30px',
-                color: 'black',
-                display: 'block',
-                fontWeight: '700',
-                minWidth: '243px',
-                padding: '18px 62px',
-                textAlign: 'center',
-            }}>Otváracie hodiny</Button>
-            <Typography fontWeight='bold' variant='h5' color='#b6490c'>Nájdete u nás:</Typography>
-            <Stack display="flex" flexDirection={isMobile ? "column" : "row"} justifyContent='center'>
-                {animalsToDisplay.map(animal => <AnimalPaper id={animal.id}
-                                                             key={animal.id}
-                                                             name={animal.name}
-                                                             shortDescription={animal.shortDescription}
-                                                             animalFile={animal.animalFile}/>)}
-            </Stack>
+            <AboutSection/>
             <Button onClick={() => navigate('/animals')} sx={{
                 bgcolor: '#f8b957',
                 borderRadius: '30px',
@@ -64,7 +44,7 @@ export const MainSection: FC = () => {
                 padding: '18px 62px',
                 textAlign: 'center',
                 marginBottom: '20px'
-            }}>Všetky zvieratá</Button>
+            }}>Prehľad zvierat</Button>
         </Stack>
     </Box>
         ;
