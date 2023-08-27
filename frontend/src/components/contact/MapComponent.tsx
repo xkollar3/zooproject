a5473a41f2b1ca1d175c5e502f2851d68a11336d
xkollar3 <xkollar3@fi.muni.cz>
2023-08-27 12:23:36 +0200

    fixed map border

--- frontend/src/components/contact/MapComponent.tsx
+++ frontend/src/components/contact/MapComponent.tsx
@@ -6,5 +6,5 @@ export const MapComponent: FC = () => {
 
     return <iframe
         src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d10649.38243693817!2d18.586229!3d48.1421419!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x476ae78b59122d53%3A0x49c5b1eee37c0bb!2sPanthera%20park!5e0!3m2!1sen!2shr!4v1693067830712!5m2!1sen!2shr"
-        width={isMediumSize ? "100%" : "60%"} height="400"></iframe>;
+        width={isMediumSize ? "100%" : "60%"} height="400" style={{border: 0}} allowFullScreen={true}></iframe>;
 }
