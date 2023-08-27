ec6ed0c8dff5f9055186dd3a9ab2d2a970563abf
xkollar3 <xkollar3@fi.muni.cz>
2023-08-27 12:19:28 +0200

    added opening hours section

--- frontend/src/components/openinghours/OpeningHoursCard.tsx
+++ frontend/src/components/openinghours/OpeningHoursCard.tsx
@@ -1,5 +1,5 @@
 import {FC} from "react";
-import {Box, Divider, Stack, Typography} from "@mui/material";
+import {Box, Divider, Stack, Typography, useMediaQuery} from "@mui/material";
 
 interface OpeningHoursCardProps {
     key: string,
@@ -20,21 +20,27 @@ interface OpeningHoursCardProps {
 }
 
 export const OpeningHoursCard: FC<OpeningHoursCardProps> = (props: OpeningHoursCardProps) => {
-    return <Box maxWidth='400px' minHeight='400px' width={props.width} style={{boxShadow: '16px 10px 17px 0px rgba(0,0,0,0.45)'}}
-                bgcolor={props.bgColor} padding='15px'>
+    const isMobile = useMediaQuery((theme: any) => theme.breakpoints.down("sm")
+        || theme.breakpoints.down("xs"));
+
+    return <Box maxWidth='400px' height='450px' width={props.width}
+                style={isMobile ? {} : {boxShadow: '16px 10px 17px 0px rgba(0,0,0,0.45)'}}
+                bgcolor={props.bgColor} padding={isMobile ? '15px 0px 15px 0px' : '15px'} alignItems='center'
+                justifyContent='center'>
         <Box width='100%' display='flex' flexDirection='column' rowGap={3}>
-            <Typography fontFamily='Anton, sans-serif' align='center' variant='h4'>{props.start} - {props.end}</Typography>
+            <Typography fontFamily='Anton, sans-serif' align='center'
+                        variant='h4'>{props.start} - {props.end}</Typography>
             <Stack alignItems='center'>
                 <Typography fontFamily='Bebas Neue, sans-serif' variant='h4'>Pracovné dni</Typography>
                 <Typography variant='h4'>{props.weekday.from} - {props.weekday.to}</Typography>
             </Stack>
-            <Divider sx={{ width: '100%' }} />
+            <Divider sx={{width: '100%'}}/>
             <Stack alignItems='center'>
                 <Typography fontFamily='Bebas Neue, sans-serif' variant='h4'>Víkend</Typography>
                 <Typography variant='h4'>{props.weekend.from} - {props.weekend.to}</Typography>
             </Stack>
             <Typography fontFamily='Bebas Neue, sans-serif' align='center' variant='h5'>{props.note1}</Typography>
-            <Typography  fontFamily='Bebas Neue, sans-serif' align='center' variant='h5'>{props.note2}</Typography>
+            <Typography fontFamily='Bebas Neue, sans-serif' align='center' variant='h5'>{props.note2}</Typography>
         </Box>
     </Box>
 }
