dcde33c948a9f15fc8d913bd3bd149217067b483
xkollar3 <xkollar3@fi.muni.cz>
2023-09-23 13:26:27 +0200

    added detail pics to some animals, changes to modal

--- frontend/src/components/catalogue/ItemModal.tsx
+++ frontend/src/components/catalogue/ItemModal.tsx
@@ -1,4 +1,4 @@
-import {CardMedia, Dialog, DialogContent, Slide} from "@mui/material";
+import {CardMedia, Dialog, DialogContent, Slide, useMediaQuery} from "@mui/material";
 import {TransitionProps} from "@mui/material/transitions";
 import React from "react";
 
@@ -19,17 +19,29 @@ interface ItemModalProps {
 }
 
 export const ItemModal = (props: ItemModalProps) => {
+    const isMobile = useMediaQuery((theme: any) => theme.breakpoints.down("sm")
+        || theme.breakpoints.down("xs"));
+
     const getPath = (badPath: string) => {
         return badPath.slice(0, badPath.length - 4) + "_detail.jpg";
     }
 
-    return <Dialog scroll='body' fullWidth={true} TransitionComponent={Transition} open={props.isOpen} onClose={props.onClose} maxWidth="md">
-        <DialogContent sx={{"padding": '10px', 'margin': '0px'}}>
+    const contentPadding = isMobile ? '10px 0' : '10px';
+
+    return <Dialog sx={{
+        '.MuiDialog-paper': {
+            'margin': '0',
+            'width' : '100%'
+        }
+    }} scroll={isMobile ? 'paper' : 'body'}
+                   fullWidth={true} TransitionComponent={Transition}
+                   open={props.isOpen} onClose={props.onClose} maxWidth={isMobile ? 'sm' : 'md'}>
+        <DialogContent sx={{"padding": contentPadding, 'margin': '0'}}>
             <CardMedia
                 component="img"
                 src={"animals/" + getPath(props.imgPath)}
                 alt={props.animal}
-                style={{ width: "100%" }}
+                style={{width: "100%"}}
             />
         </DialogContent>
     </Dialog>
